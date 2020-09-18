b862e2de6b0291762859f67cc6b2beb7d3eba017
Jacob Kagon <jacob@kagon.net>
2020-09-18 23:53:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -38,7 +38,7 @@ def find_valid_calls(planeteer_calls)
  
  new_array = []
  planeteer_calls.each do |element| 
-   if element = valid_calls
+   if element = valid_calls[0]
     new_array << element
    end
   end
