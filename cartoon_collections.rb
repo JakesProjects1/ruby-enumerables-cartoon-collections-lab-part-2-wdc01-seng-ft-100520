cbfc69c932f12159f106da3240479ac4f276ce13
Jacob Kagon <jacob@kagon.net>
2020-09-18 23:19:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -35,7 +35,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   
-  planeteer_calls.find do |element|
+  planeteer_calls.include? do |element|
     if element = valid_calls
       element
     end
