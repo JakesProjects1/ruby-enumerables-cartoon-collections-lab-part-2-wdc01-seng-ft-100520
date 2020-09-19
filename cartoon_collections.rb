5071b9408cc3496bb4202a8709ba4afde338deeb
Jacob Kagon <jacob@kagon.net>
2020-09-19 00:03:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -41,9 +41,9 @@ def find_valid_calls(planeteer_calls)
    if valid_calls[0] = planeteer_calls[0]
      new_arrays << valid_calls[0]
    end 
-end
+ end
   
-   new_array
+new_array
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
 end
